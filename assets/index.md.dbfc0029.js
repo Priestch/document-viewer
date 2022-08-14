ca50040ee55edc2a01bfa5f06353e14cee2c1b6f
Priestch <Priestch@users.noreply.github.com>
2022-08-14 09:50:07 +0000

    deploy: 0109ed2e71f40649f9f5026a5b440f9f24a9815f

---
+++ assets/index.md.dbfc0029.js
@@ -0,0 +1,7 @@
+import{_ as e,c as a,o as n,a as s}from"./app.2abf7d09.js";const u=JSON.parse('{"title":"DocumentViewer","description":"","frontmatter":{},"headers":[{"level":2,"title":"Getting Started","slug":"getting-started"}],"relativePath":"index.md"}'),t={name:"index.md"},p=s(`<h1 id="documentviewer" tabindex="-1">DocumentViewer <a class="header-anchor" href="#documentviewer" aria-hidden="true">#</a></h1><p>An out-of-the-box PDF viewer based on pdf.js.</p><h2 id="getting-started" tabindex="-1">Getting Started <a class="header-anchor" href="#getting-started" aria-hidden="true">#</a></h2><div class="language-bash"><span class="copy"></span><pre><code><span class="line"><span style="color:#A6ACCD;">npm create @document-kits/viewer@latest my-app</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#82AAFF;">cd</span><span style="color:#A6ACCD;"> my-app</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;">npm install</span></span>
+<span class="line"><span style="color:#A6ACCD;">npm run dev</span></span>
+<span class="line"></span></code></pre></div>`,4),r=[p];function c(i,o,l,d,_,h){return n(),a("div",null,r)}var A=e(t,[["render",c]]);export{u as __pageData,A as default};

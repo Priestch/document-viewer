802a7c7db90843cba3a58201da727360c2a50844
Priestch <priestch@gmail.com>
2023-08-24 22:20:20 +0800

    docs: use google tag manager

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,8 +1,9 @@
 const scriptContent = `
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);};
-  gtag('js', new Date());
-  gtag('config', 'G-6648ZRLKLT');
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N4MC8X9M');
 `;
 
 /**
@@ -18,10 +19,7 @@ const config = {
   markdown: {
     lineNumbers: false,
   },
-  head: [
-    ["script", { src: "https://www.googletagmanager.com/gtag/js?id=G-6648ZRLKLT", async: true }],
-    ["script", {}, scriptContent],
-  ],
+  head: [["script", {}, scriptContent]],
   themeConfig: {
     nav: [
       { text: "Home", link: "/" },

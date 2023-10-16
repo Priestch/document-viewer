53d100c4dd98a51d62425008cbc96f93c1942cd4
Priestch <priestch@gmail.com>
2023-10-17 00:21:26 +0800

    demos: fix build

--- demos/vite.config.js
+++ demos/vite.config.js
@@ -1,29 +1,31 @@
 import { viteStaticCopy } from "vite-plugin-static-copy";
-import { resolve } from "path";
-import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    viteStaticCopy({
-      targets: [
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  const base = command === "build" ? "/document-viewer/demos/" : "/";
+  return {
+    base,
+    plugins: [
+      vue(),
+      viteStaticCopy({
+        targets: [
+          {
+            src: "node_modules/@document-kits/viewer/dist/generic/*",
+            dest: "document-viewer",
+          },
+        ],
+      }),
+    ],
+    resolve: {
+      alias: [
         {
-          src: "node_modules/@document-kits/viewer/dist/generic/*",
-          dest: "document-viewer",
+          find: /^canvas$/,
+          replacement: "canvas.js",
         },
       ],
-    }),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: /^canvas$/,
-        replacement: "canvas.js",
-      },
-    ],
-  },
+    },
+  };
 });

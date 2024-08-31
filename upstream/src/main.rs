0cca80c03a73f1d1e25c2baaf36334d7399e44e0
Priestch <priestch@gmail.com>
2024-08-31 10:08:57 +0800

    refactor: move utils function gen to independent module

--- upstream/src/main.rs
+++ upstream/src/main.rs
@@ -1,20 +1,25 @@
 #![feature(allocator_api)]
 
+mod utils_gen;
+
+use crate::utils_gen::UtilsGen;
 use oxc::allocator::{Allocator, CloneIn, Vec as OxcVec};
-use oxc::ast::ast::{BindingPattern, BindingPatternKind, BindingRestElement, ClassElement, Declaration, Expression, Function, FunctionBody, FunctionType, IfStatement, ImportOrExportKind, ModuleDeclaration, ModuleExportName, ObjectExpression, ObjectProperty, PropertyKind, Statement, TSTypeAnnotation, TSTypeParameterDeclaration, VariableDeclarationKind};
+use oxc::ast::ast::{
+    BindingPattern, BindingPatternKind, ClassElement, Declaration, Expression, Function,
+    FunctionBody, ModuleDeclaration, ObjectExpression, ObjectProperty, PropertyKind, Statement,
+};
 use oxc::ast::visit::walk;
-use oxc::ast::visit::walk::{walk_if_statement, walk_module_declaration, walk_statement};
-use oxc::ast::{ast, match_declaration, match_expression, AstBuilder, AstKind, Visit};
+use oxc::ast::visit::walk::{walk_module_declaration, walk_statement};
+use oxc::ast::{match_declaration, AstBuilder, AstKind, Visit};
 use oxc::codegen::{Codegen, Context, Gen, GenExpr};
 use oxc::parser::Parser;
-use oxc::span::{Atom, CompactStr, GetSpan, SourceType, Span, SPAN};
+use oxc::span::{CompactStr, GetSpan, SourceType, Span};
 use oxc::syntax::precedence::Precedence;
-use oxc::syntax::scope::ScopeFlags;
 use std::borrow::Cow;
 use std::cell::RefCell;
 use std::cmp::min;
 use std::hash::Hash;
-use std::{env, fs, vec};
+use std::{env, fs};
 
 struct ObjProperty {
     name: CompactStr,
@@ -39,7 +44,6 @@ struct AppExtractor<'a> {
     in_app_variable_context: bool,
     app_visited: bool,
     app_codegen: RefCell<Codegen<'a>>,
-    helper_codegen: RefCell<Codegen<'a>>,
     app_builder: AstBuilder<'a>,
     app_body: OxcVec<'a, ClassElement<'a>>,
     visiting_prop: Option<ObjProperty>,
@@ -47,9 +51,8 @@ struct AppExtractor<'a> {
     object_property_depth: u32,
     func_declare_depth: u32,
     object_expression_depth: u32,
-    helper_body: FunctionBody<'a>,
     in_target_if_block: bool,
-    func_names_in_helper: OxcVec<'a, Atom<'a>>,
+    pub utils_gen: UtilsGen<'a>,
 }
 
 impl<'a> AppExtractor<'a> {
@@ -59,170 +62,25 @@ impl<'a> AppExtractor<'a> {
         let builder = AstBuilder::new(allocator);
         let app_body = OxcVec::new_in(allocator);
         let codegen = Codegen::new();
-        let helper_codegen = Codegen::new();
-        // let comment_options = CommentOptions {
-        //     preserve_annotate_comments: true
-        // };
-        // helper_codegen = helper_codegen.enable_comment("", Trivias::default(), comment_options);
-        let mut statements = builder.vec();
-        let property = builder.binding_property(
-            SPAN,
-            builder.property_key_identifier_name(SPAN, Atom::from("AppOptions")),
-            builder.binding_pattern(
-                builder.binding_pattern_kind_binding_identifier(
-                    SPAN,
-                    Atom::from("AppOptions"),
-                ),
-                None::<TSTypeAnnotation>,
-                false,
-            ),
-            true,
-            false,
-        );
-        let init = builder.expression_identifier_reference(
-            SPAN,
-            Atom::from("PDFViewerApplication"),
-        );
-        let declaration = builder.declaration_from_variable(
-            builder.variable_declaration(
-                SPAN,
-                VariableDeclarationKind::Const,
-                builder.vec1(
-                    builder.variable_declarator(
-                        SPAN,
-                        VariableDeclarationKind::Const,
-                        builder.binding_pattern(
-                            builder.binding_pattern_kind_object_pattern(
-                                SPAN,
-                                builder.vec1(property),
-                                None::<BindingRestElement>,
-                            ),
-                            None::<TSTypeAnnotation>,
-                            false,
-                        ),
-                        Some(init),
-                        false,
-                    )
-                ),
-                false,
-            )
-        );
-        statements.push(
-            Statement::from(declaration)
-        );
         Self {
             in_app_variable_context: false,
             app_visited: false,
             app_codegen: RefCell::new(codegen),
-            helper_codegen: RefCell::new(helper_codegen),
             app_builder: builder,
             app_body,
-            helper_body: builder.function_body(SPAN, builder.vec(), statements),
             visiting_prop: None,
             source_text: source_text.to_string(),
             object_property_depth: 0,
             func_declare_depth: 0,
             object_expression_depth: 0,
             in_target_if_block: false,
-            func_names_in_helper: OxcVec::from_iter_in(vec![
-                Atom::from("validateFileURL"),
-                Atom::from("webViewerFileInputChange"),
-            ], builder.allocator),
+            utils_gen: UtilsGen::new(builder.allocator),
         }
     }
 
-    fn get_helper_func_text(&mut self) -> String {
-        let mut properties = OxcVec::with_capacity_in(self.func_names_in_helper.len(), self.app_builder.allocator);
-
-        for name in &self.func_names_in_helper {
-            let s = name.as_str();
-            let property = self.app_builder.object_property(
-                SPAN,
-                PropertyKind::Init,
-                self.app_builder.property_key_identifier_name(SPAN, Atom::from(s)),
-                self.app_builder.expression_identifier_reference(SPAN, Atom::from(s)),
-                None,
-                false,
-                true,
-                false,
-            );
-
-            properties.push(
-                self.app_builder.object_property_kind_from_object_property(property)
-            )
-        }
-
-        let statement = self.app_builder.return_statement(
-            SPAN,
-            Some(
-                self.app_builder.expression_from_object(
-                    self.app_builder.object_expression(
-                        SPAN,
-                        properties,
-                        None,
-                    )
-                )
-            ),
-        );
-        self.helper_body.statements.push(
-            self.app_builder.statement_from_return(statement)
-        );
-
-        let body = self.helper_body.clone_in(self.app_builder.allocator);
-        let id = self.app_builder.binding_identifier(SPAN, Atom::from("createHelper"));
-        let parameters = self.app_builder.formal_parameters(
-            SPAN,
-            ast::FormalParameterKind::Signature,
-            self.app_builder.vec1(self.app_builder.formal_parameter(
-                SPAN,
-                self.app_builder.vec(),
-                self.app_builder.binding_pattern(
-                    self.app_builder.binding_pattern_kind_binding_identifier(SPAN, "PDFViewerApplication"),
-                    None::<TSTypeAnnotation>,
-                    false,
-                ),
-                None,
-                false,
-                false,
-            )),
-            None::<BindingRestElement>,
-        );
-
-        let function = self.app_builder.function(
-            FunctionType::FunctionDeclaration,
-            SPAN,
-            Some(id),
-            false,
-            false,
-            false,
-            None::<TSTypeParameterDeclaration>,
-            None,
-            parameters,
-            None::<TSTypeAnnotation>,
-            Some(body),
-        );
-
-        let codegen = self.helper_codegen.get_mut();
-        let context = Context::default();
-        function.gen(codegen, context);
-
-        let export_stmt = self.app_builder.module_declaration_export_named_declaration(
-            SPAN,
-            None,
-            self.app_builder.vec1(
-                self.app_builder.export_specifier(
-                    SPAN,
-                    ModuleExportName::IdentifierName(self.app_builder.identifier_name(SPAN, Atom::from("createHelper"))),
-                    ModuleExportName::IdentifierName(self.app_builder.identifier_name(SPAN, Atom::from("createHelper"))),
-                    ImportOrExportKind::Value,
-                )
-            ),
-            None,
-            ImportOrExportKind::Value,
-            None
-        );
-
-        Statement::from(export_stmt).gen(codegen, context);
+    fn get_app_module_text(&mut self) -> String {
+        self.print_class_end();
+        let codegen = self.app_codegen.get_mut();
 
         codegen.into_source_text()
     }
@@ -328,10 +186,12 @@ impl Visit<'_> for AppExtractor<'_> {
                 self.in_target_if_block = true;
                 let span = ifs.test.span();
                 let test_text = self.get_span_text(span, span.end as usize);
-                if self.app_visited && if_text == test_text && self.func_declare_depth == 0{
-                    self.helper_body.statements.push(it.clone_in(self.app_builder.allocator));
+                if self.app_visited && if_text == test_text && self.func_declare_depth == 0 {
+                    // self.helper_body.statements.push(it.clone_in(self.app_builder.allocator));
+                    self.utils_gen
+                        .add_func_statement(it.clone_in(self.app_builder.allocator));
                 }
-            },
+            }
             // Statement::LabeledStatement(it) => visitor.visit_labeled_statement(it),
             // Statement::ReturnStatement(it) => visitor.visit_return_statement(it),
             // Statement::SwitchStatement(it) => visitor.visit_switch_statement(it),
@@ -348,20 +208,24 @@ impl Visit<'_> for AppExtractor<'_> {
                         if self.app_visited
                             && self.func_declare_depth == 0
                             && !self.in_target_if_block
-                            && self.object_expression_depth == 0 {
+                            && self.object_expression_depth == 0
+                        {
                             if let Some(id) = &func.id {
                                 let identifier = id.name.as_str();
-                                self.func_names_in_helper.push(self.app_builder.atom(identifier));
-                                self.helper_body.statements.push(it.clone_in(self.app_builder.allocator));
+                                // self.func_names_in_helper.push(self.app_builder.atom(identifier));
+                                self.utils_gen
+                                    .add_func_name(self.app_builder.atom(identifier));
+                                // self.helper_body.statements.push(it.clone_in(self.app_builder.allocator));
+                                self.utils_gen
+                                    .add_func_statement(it.clone_in(self.app_builder.allocator));
                             }
-
                         }
                         self.func_declare_depth += 1;
                     }
                     // Declaration::ClassDeclaration(it) => visitor.visit_class(it),
                     _ => {}
                 }
-            },
+            }
             // match_module_declaration!(Statement) => {
             //     visitor.visit_module_declaration(it.to_module_declaration())
             _ => {}
@@ -375,7 +239,6 @@ impl Visit<'_> for AppExtractor<'_> {
         if is_func_stmt {
             self.func_declare_depth -= 1;
         }
-
     }
 
     fn visit_expression(&mut self, expr: &Expression<'_>) {
@@ -452,7 +315,7 @@ impl Visit<'_> for AppExtractor<'_> {
     fn visit_module_declaration(&mut self, it: &ModuleDeclaration<'_>) {
         match it {
             ModuleDeclaration::ImportDeclaration(import) => {
-                import.gen(self.helper_codegen.get_mut(), Context::default());
+                self.utils_gen.gen_import_declaration(import);
             }
             _ => {}
         }
@@ -475,14 +338,12 @@ fn main() {
 
     app.visit_program(&ret.program);
 
-    app.print_class_end();
-    let codegen = app.app_codegen.get_mut();
-    let generated = codegen.into_source_text();
+    let generated = app.get_app_module_text();
     let output = cwd.join("upstream/output.js");
     fs::write(&output, generated);
 
     let helper = cwd.join("upstream/helper.js");
-    let helper_source = app.get_helper_func_text();
+    let helper_source = app.utils_gen.get_module_text();
     fs::write(&helper, helper_source);
 
     // let allocator = Allocator::default();
---
+++ upstream/src/utils_gen.rs
@@ -0,0 +1,189 @@
+use std::cell::RefCell;
+use oxc::allocator;
+use oxc::allocator::{Allocator, CloneIn};
+use oxc::ast::ast::{BindingRestElement, FunctionBody, FunctionType, ImportDeclaration, ImportOrExportKind, ModuleExportName, PropertyKind, Statement, TSTypeAnnotation, TSTypeParameterDeclaration, VariableDeclarationKind};
+use oxc::ast::{ast, AstBuilder};
+use oxc::codegen::{Codegen, Context, Gen};
+use oxc::span::{Atom, SPAN};
+
+pub struct UtilsGen<'a> {
+    codegen: RefCell<Codegen<'a>>,
+    builder: AstBuilder<'a>,
+    helper_body: FunctionBody<'a>,
+    func_names_in_helper: allocator::Vec<'a, Atom<'a>>,
+}
+
+impl<'a> UtilsGen<'a> {
+    pub fn new(allocator: &'a Allocator) -> Self {
+        let builder = AstBuilder::new(allocator);
+        let codegen = Codegen::new();
+        // let comment_options = CommentOptions {
+        //     preserve_annotate_comments: true
+        // };
+        // helper_codegen = helper_codegen.enable_comment("", Trivias::default(), comment_options);
+        let mut statements = builder.vec();
+        let property = builder.binding_property(
+            SPAN,
+            builder.property_key_identifier_name(SPAN, Atom::from("AppOptions")),
+            builder.binding_pattern(
+                builder.binding_pattern_kind_binding_identifier(
+                    SPAN,
+                    Atom::from("AppOptions"),
+                ),
+                None::<TSTypeAnnotation>,
+                false,
+            ),
+            true,
+            false,
+        );
+        let init = builder.expression_identifier_reference(
+            SPAN,
+            Atom::from("PDFViewerApplication"),
+        );
+        let declaration = builder.declaration_from_variable(
+            builder.variable_declaration(
+                SPAN,
+                VariableDeclarationKind::Const,
+                builder.vec1(
+                    builder.variable_declarator(
+                        SPAN,
+                        VariableDeclarationKind::Const,
+                        builder.binding_pattern(
+                            builder.binding_pattern_kind_object_pattern(
+                                SPAN,
+                                builder.vec1(property),
+                                None::<BindingRestElement>,
+                            ),
+                            None::<TSTypeAnnotation>,
+                            false,
+                        ),
+                        Some(init),
+                        false,
+                    )
+                ),
+                false,
+            )
+        );
+        statements.push(
+            Statement::from(declaration)
+        );
+
+        Self {
+            codegen: RefCell::new(codegen),
+            builder,
+            helper_body: builder.function_body(SPAN, builder.vec(), statements),
+            func_names_in_helper: allocator::Vec::from_iter_in(vec![
+                Atom::from("validateFileURL"),
+                Atom::from("webViewerFileInputChange"),
+            ], builder.allocator),
+        }
+    }
+
+    pub fn get_module_text(&mut self) -> String {
+        let mut properties = allocator::Vec::with_capacity_in(self.func_names_in_helper.len(), self.builder.allocator);
+
+        for name in &self.func_names_in_helper {
+            let s = name.as_str();
+            let property = self.builder.object_property(
+                SPAN,
+                PropertyKind::Init,
+                self.builder.property_key_identifier_name(SPAN, Atom::from(s)),
+                self.builder.expression_identifier_reference(SPAN, Atom::from(s)),
+                None,
+                false,
+                true,
+                false,
+            );
+
+            properties.push(
+                self.builder.object_property_kind_from_object_property(property)
+            )
+        }
+
+        let statement = self.builder.return_statement(
+            SPAN,
+            Some(
+                self.builder.expression_from_object(
+                    self.builder.object_expression(
+                        SPAN,
+                        properties,
+                        None,
+                    )
+                )
+            ),
+        );
+        self.helper_body.statements.push(
+            self.builder.statement_from_return(statement)
+        );
+
+        let body = self.helper_body.clone_in(self.builder.allocator);
+        let id = self.builder.binding_identifier(SPAN, Atom::from("createHelper"));
+        let parameters = self.builder.formal_parameters(
+            SPAN,
+            ast::FormalParameterKind::Signature,
+            self.builder.vec1(self.builder.formal_parameter(
+                SPAN,
+                self.builder.vec(),
+                self.builder.binding_pattern(
+                    self.builder.binding_pattern_kind_binding_identifier(SPAN, "PDFViewerApplication"),
+                    None::<TSTypeAnnotation>,
+                    false,
+                ),
+                None,
+                false,
+                false,
+            )),
+            None::<BindingRestElement>,
+        );
+
+        let function = self.builder.function(
+            FunctionType::FunctionDeclaration,
+            SPAN,
+            Some(id),
+            false,
+            false,
+            false,
+            None::<TSTypeParameterDeclaration>,
+            None,
+            parameters,
+            None::<TSTypeAnnotation>,
+            Some(body),
+        );
+
+        let codegen = self.codegen.get_mut();
+        let context = Context::default();
+        function.gen(codegen, context);
+
+        let export_stmt = self.builder.module_declaration_export_named_declaration(
+            SPAN,
+            None,
+            self.builder.vec1(
+                self.builder.export_specifier(
+                    SPAN,
+                    ModuleExportName::IdentifierName(self.builder.identifier_name(SPAN, Atom::from("createHelper"))),
+                    ModuleExportName::IdentifierName(self.builder.identifier_name(SPAN, Atom::from("createHelper"))),
+                    ImportOrExportKind::Value,
+                )
+            ),
+            None,
+            ImportOrExportKind::Value,
+            None,
+        );
+
+        Statement::from(export_stmt).gen(codegen, context);
+
+        codegen.into_source_text()
+    }
+
+    pub fn add_func_statement(&mut self, statement: Statement<'a>) {
+        self.helper_body.statements.push(statement)
+    }
+
+    pub fn add_func_name(&mut self, name: Atom<'a>) {
+        self.func_names_in_helper.push(name)
+    }
+
+    pub fn gen_import_declaration(&mut self, it: &allocator::Box<ImportDeclaration>) {
+        it.gen(self.codegen.get_mut(), Context::default());
+    }
+}
